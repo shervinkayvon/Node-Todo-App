22d7455606c9e7047785ccefc8a3932f2eabda15
Shervin Kayvon <shervinkayvon@gmail.com>
2018-12-06 13:54:48 +0100

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const _ = require('lodash');
 const { ObjectID } = require('mongodb');
+const bcrypt = require('bcryptjs');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
@@ -112,6 +113,18 @@ app.post('/users', (req, res) => {
 
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => {
+    const body = _.pick(req.body, ['email', 'password']);
+    
+    User.findByCredentials(body.email, body.password).then(user => {
+        return user.generateAuthToken().then(token => {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch(e => {
+        res.status(400).send();
+    });
 });
 
 app.listen(port, () => {
---
+++ server/tests/seed/seed.js
@@ -0,0 +1,54 @@
+const { ObjectID } = require('mongodb');
+const jwt = require('jsonwebtoken');
+
+const { Todo } = require('./../../models/todo');
+const { User } = require('./../../models/user');
+
+const userOneId = new ObjectID();
+const userTwoId = new ObjectID();
+const users = [
+    {
+        _id: userOneId,
+        email: 'shervinkayvon@gmail.com',
+        password: '123abc',
+        tokens: [{
+            access: 'auth',
+            token: jwt.sign({ _id: userOneId, access: 'auth' }, 'oursecretsalt').toString()
+        }]
+    },
+    {
+        _id: userTwoId,
+        email: 'me@gmail.com',
+        password: '123abc'
+    }
+];
+
+const todos = [
+    {
+        _id: new ObjectID(), 
+        text: 'Here is some text' 
+    },
+    { 
+        _id: new ObjectID(), 
+        text: 'Here is more text',
+        completed: true,
+        completedAt: 333
+    }
+];
+
+const populateTodos = done => {
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(todos);
+    }).then(() => done());
+};
+
+const populateUsers = done => {
+    User.remove({}).then(() => {
+        const userOne = new User(users[0]).save();
+        const userTwo = new User(users[1]).save();
+        
+        return Promise.all([userOne, userTwo]);
+    }).then(() => done());
+};
+
+module.exports = { todos, populateTodos, users, populateUsers };
